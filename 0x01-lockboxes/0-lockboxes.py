3fe7b4d9f2b9a9f732525b721cf0c492053d87aa
martinopolot <maopolot@outlook.com>
2023-07-05 07:52:49 +0000

    updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,14 +1,48 @@
+#!/usr/bin/python3
+""" defines method to solve lockboxes problem """
+
+
 def canUnlockAll(boxes):
-    num_of_boxes = len(boxes)
-    unlocked = [False] * num_of_boxes
-    unlocked[0] = True
-    stack = [0]
+    from copy import deepcopy
 
-    while stack:
-        box = stack.pop()
-        for key in boxes[box]:
-            if key < num_of_boxes and not unlocked[key]:
-                unlocked[key] = True
-                stack.append(key)
+    # checks if given valid list of boxes
+    if type(boxes) is not list or len(boxes) < 1:
+        return False
+    for box in boxes:
+        if type(box) is not list:
+            return False
+    # creates a copy of boxes to not affect the original list of lists
+    copy_boxes = deepcopy(boxes)
+    # creates list of available keys (only contains 0 initially)
+    keys_list = [0]
+    # while there are still available keys:
+    while len(keys_list) > 0:
+        # the current key will be the first available key
+        key = keys_list[0]
+        # keys list will reset to remove used key
+        keys_list = keys_list[1:]
+        if type(key) is int and key >= 0:
+            copy_boxes[key].append(-1)
+            for new_key in copy_boxes[key]:
+                if new_key == -1:
+                    # if new key is -1 flag to marks as opened, continue
+                    continue
+                if new_key >= len(copy_boxes):
+                    # if new key is out of range for the boxes, continue
+                    continue
+                if -1 in copy_boxes[new_key] or new_key in keys_list:
+                    # if box key is known, continue
+                    continue
+                # update the list of available key
+                assert isinstance(new_key, object)
+                keys_list.append(new_key)
+            continue
+        # checks if given valid key
+        return False
+    for box in copy_boxes:
+        if -1 not in box:
+            # if a box is missing the -1 flag, it was not unlocked
+            return False
+    # returns true if all boxes in the previous loop were indicated as unlocked
+    return True
 
-    return all(unlocked)
